8b00af16068dc0ee7b654ff3f342571b742d6f39
vagrant <vagrant@kleer-csd-box>
2017-04-21 16:56:31 +0200

    fin sprint 2

--- app.rb
+++ app.rb
@@ -1,8 +1,21 @@
 require 'sinatra'
 
-
 @@palabra = "A"
 
+class Diccionario
+  
+  def generarLetra
+    @@palabra
+  end
+  def validar (letra)
+    if @@palabra == letra.to_s
+      return true
+    else false
+    end
+  end
+
+end
+ 
 get '/' do
 	erb :index
 end
